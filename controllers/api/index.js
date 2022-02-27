3a5400541c7856905f114ce651293a3571a1fe16
Spawn43 <62122244+Spawn43@users.noreply.github.com>
2022-02-27 23:37:34 +0000

    added routes to the table

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-
+const tableRoutes = require("./table-routes");
 const userRoutes = require("./user-routes");
 
 
+router.use("/table", tableRoutes);
 router.use("/user", userRoutes);
 
 module.exports = router;
